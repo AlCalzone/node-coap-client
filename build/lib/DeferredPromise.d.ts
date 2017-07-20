539fcafc57b812551e3f4860e5033d88c0c0bdad
Dominic Griesel <d.griesel@gmx.net>
2017-07-20 20:30:43 +0200

    cannot use a subclass of Promise for async code :(

--- build/lib/DeferredPromise.d.ts
+++ build/lib/DeferredPromise.d.ts
@@ -1,16 +1,5 @@
-/**
- * Superset of the promise class that allows to manually resolve or reject the promise
- */
-export declare class DeferredPromise<T> extends Promise<T> {
-    private res;
-    /**
-     * Resolve the promise with the given value
-     */
+export interface DeferredPromise<T> extends Promise<T> {
     resolve(value?: T | PromiseLike<T>): void;
-    private rej;
-    /**
-     * Resolve the promise with the given reason
-     */
     reject(reason?: any): void;
-    constructor();
 }
+export declare function createDeferredPromise<T>(): DeferredPromise<T>;
--- src/lib/DeferredPromise.ts
+++ src/lib/DeferredPromise.ts
@@ -1,30 +1,18 @@
-﻿/**
- * Superset of the promise class that allows to manually resolve or reject the promise
- */
-export class DeferredPromise<T> extends Promise<T> {
+﻿export interface DeferredPromise<T> extends Promise<T> {
+	resolve(value?: T | PromiseLike<T>): void;
+	reject(reason?: any): void;
+}
 
-	private res: (value?: T | PromiseLike<T>) => void;
-	/**
-	 * Resolve the promise with the given value
-	 */
-	resolve(value?: T | PromiseLike<T>): void {
-		this.res(value);
-	}
+export function createDeferredPromise<T>(): DeferredPromise<T> {
+	let res, rej;
 
-	private rej: (reason?: any) => void;
-	/**
-	 * Resolve the promise with the given reason
-	 */
-	reject(reason?: any): void {
-		this.rej(reason);
-	}
+	const promise = new Promise<T>((resolve, reject) => {
+		res = resolve;
+		rej = reject;
+	}) as DeferredPromise<T>;
 
-	constructor() {
-		// remember the resolve and reject functions so we can manually call them
-		super((resolve, reject) => {
-			this.res = resolve;
-			this.rej = reject;
-		});
-	}
+	promise.resolve = res;
+	promise.reject = rej;
 
+	return promise;
 }
