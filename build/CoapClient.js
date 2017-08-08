2caf0b1373f0982778b1941477a0b840548c5ed7
Dominic Griesel <d.griesel@gmx.net>
2017-08-08 23:06:10 +0200

    built the files after including tslint

--- build/CoapClient.js
+++ build/CoapClient.js
@@ -8,8 +8,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t;
+    return { next: verb(0), "throw": verb(1), "return": verb(2) };
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -556,15 +556,15 @@ var CoapClient = (function () {
                 throw new Error("protocol type \"" + origin.protocol + "\" is not supported");
         }
     };
-    /** Table of all open connections and their parameters, sorted by the origin "coap(s)://host:port" */
-    CoapClient.connections = {};
-    /** Table of all known security params, sorted by the hostname */
-    CoapClient.dtlsParams = {};
-    /** All pending requests, sorted by the token */
-    CoapClient.pendingRequestsByToken = {};
-    CoapClient.pendingRequestsByMsgID = {};
-    CoapClient.pendingRequestsByUrl = {};
     return CoapClient;
 }());
+/** Table of all open connections and their parameters, sorted by the origin "coap(s)://host:port" */
+CoapClient.connections = {};
+/** Table of all known security params, sorted by the hostname */
+CoapClient.dtlsParams = {};
+/** All pending requests, sorted by the token */
+CoapClient.pendingRequestsByToken = {};
+CoapClient.pendingRequestsByMsgID = {};
+CoapClient.pendingRequestsByUrl = {};
 exports.CoapClient = CoapClient;
 //# sourceMappingURL=CoapClient.js.map
