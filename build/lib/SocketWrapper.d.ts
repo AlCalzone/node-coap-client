fe213c384073d1ff58ba9b14de885f633601bdc0
Björn Enochsson <bjorn.enochsson@easytelecom.se>
2017-08-21 11:04:16 +0200

    Add checks in SocketWrapper to make sure we're not trying to send to a closed socket

--- build/lib/SocketWrapper.d.ts
+++ build/lib/SocketWrapper.d.ts
@@ -6,6 +6,7 @@ import { Origin } from "./Origin";
 export declare class SocketWrapper extends EventEmitter {
     socket: dtls.Socket | dgram.Socket;
     private isDtls;
+    private isClosed;
     constructor(socket: dtls.Socket | dgram.Socket);
     send(msg: Buffer, origin: Origin): void;
     close(): void;
