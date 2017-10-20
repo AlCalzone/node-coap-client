94a1e1547204973b9c697a0167ff9b96106970ea
dominic.griesel <d.griesel@gmx.net>
2017-10-20 10:15:34 +0200

    removed sourcemaps to clean up future commits

--- build/ContentFormats.js
+++ build/ContentFormats.js
@@ -9,4 +9,3 @@ var ContentFormats;
     ContentFormats[ContentFormats["application_exi"] = 47] = "application_exi";
     ContentFormats[ContentFormats["application_json"] = 50] = "application_json";
 })(ContentFormats = exports.ContentFormats || (exports.ContentFormats = {}));
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiQ29udGVudEZvcm1hdHMuanMiLCJzb3VyY2VSb290IjoiQzovVXNlcnMvRG9taW5pYy9Eb2N1bWVudHMvVmlzdWFsIFN0dWRpbyAyMDE3L1JlcG9zaXRvcmllcy9ub2RlLWNvYXAtY2xpZW50L3NyYy8iLCJzb3VyY2VzIjpbIkNvbnRlbnRGb3JtYXRzLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUEsSUFBWSxjQU9YO0FBUEQsV0FBWSxjQUFjO0lBQ3pCLCtEQUFjLENBQUE7SUFDZCx3RkFBNEIsQ0FBQTtJQUM1QiwwRUFBNEIsQ0FBQTtJQUM1QiwwRkFBNEIsQ0FBQTtJQUM1QiwwRUFBNEIsQ0FBQTtJQUM1Qiw0RUFBNEIsQ0FBQTtBQUM3QixDQUFDLEVBUFcsY0FBYyxHQUFkLHNCQUFjLEtBQWQsc0JBQWMsUUFPekIifQ==
